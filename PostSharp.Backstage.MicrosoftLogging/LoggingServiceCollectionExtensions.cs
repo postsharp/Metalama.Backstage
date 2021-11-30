4e1628f4d3442d2612421819c1b14eb4534d9a71
Gael Fraiteur <gael@postsharp.net>
2021-11-30 06:53:26 +0100

    Added missing files.

---
+++ PostSharp.Backstage.MicrosoftLogging/LoggingServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using PostSharp.Backstage.Extensibility;
+using IMicrosoftLoggerFactory = Microsoft.Extensions.Logging.ILoggerFactory;
+using IPostSharpLoggerFactory = PostSharp.Backstage.Extensibility.ILoggerFactory;
+
+namespace PostSharp.Backstage.MicrosoftLogging
+{
+    public static class LoggingServiceCollectionExtensions
+    {
+        public static BackstageServiceCollection AddMicrosoftLoggerFactory(
+            this BackstageServiceCollection serviceCollection, IMicrosoftLoggerFactory microsoftLoggerFactory)
+            => serviceCollection.AddSingleton<IPostSharpLoggerFactory>(new LoggerFactoryAdapter(microsoftLoggerFactory));
+    }
+}
