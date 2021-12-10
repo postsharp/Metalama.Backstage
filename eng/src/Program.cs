b3fb6b5bbbad8b6ca5645b0be0d45dfedd384408
Antonín Procházka <antonin@postsharp.net>
2021-12-10 12:39:13 +0100

    #29560 PostSharp Engineering partially updated.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,50 +1,26 @@
 // Copyright (c) SharpCrafters s.r.o. All rights reserved.
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
+using System.Collections.Immutable;
 using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
-using System.Collections.Immutable;
 
-namespace BuildMetalama
+var product = new Product
 {
-    internal class Program
-    {
-        private static int Main( string[] args )
-        {
-            var privateSource = new NugetSource( "%INTERNAL_NUGET_PUSH_URL%", "%INTERNAL_NUGET_API_KEY%" );
-            var publicSource = new NugetSource( "https://api.nuget.org/v3/index.json", "%NUGET_ORG_API_KEY%" );
+    ProductName = "PostSharp.Backstage.Settings",
+    Solutions = ImmutableArray.Create<Solution>(
+                    new DotNetSolution("PostSharp.Backstage.Settings.sln") { SupportsTestCoverage = true, CanFormatCode = true }),
+    PublicArtifacts = Pattern.Create(
+        "PostSharp.Backstage.Settings.$(PackageVersion).nupkg",
+        "PostSharp.Cli.$(PackageVersion).nupkg"),
+    Dependencies = ImmutableArray.Create(
+        Dependencies.PostSharpEngineering)
+};
 
-            // These packages are published to internal and private feeds.
-            var publicPackages = new ParametricString[]
-            {
-                "PostSharp.Backstage.Settings.$(PackageVersion).nupkg",
-                "PostSharp.Cli.$(PackageVersion).nupkg"
-            };
+var commandApp = new CommandApp();
 
-            var privatePublishing = new NugetPublishTarget(
-                Pattern.Empty.Add( publicPackages ),
-                privateSource);
+commandApp.AddProductCommands(product);
 
-            var publicPublishing = new NugetPublishTarget(
-                Pattern.Empty.Add( publicPackages ),
-                privateSource,
-                publicSource );
-
-            // Currently we have no private package in this product.
-
-            var product = new Product
-            {
-                ProductName = "PostSharp.Backstage.Settings",
-                Solutions = ImmutableArray.Create<Solution>(
-                    new DotNetSolution( "PostSharp.Backstage.Settings.sln" ) { SupportsTestCoverage = true, CanFormatCode = true } ),
-                PublishingTargets = ImmutableArray.Create<PublishingTarget>( publicPublishing, privatePublishing ),
-                Dependencies = ImmutableArray<ProductDependency>.Empty
-            };
-            var commandApp = new CommandApp();
-            commandApp.AddProductCommands( product );
-
-            return commandApp.Run( args );
-        }
-    }
-}
+return commandApp.Run(args);
