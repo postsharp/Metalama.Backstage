4d70977825f7a9c541c4c37837c11529d78115c4
Antonín Procházka <antonin@postsharp.net>
2021-12-10 12:41:41 +0100

    #29560 Engineering formatting.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,26 +1,26 @@
 // Copyright (c) SharpCrafters s.r.o. All rights reserved.
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
-using System.Collections.Immutable;
 using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
+using System.Collections.Immutable;
 
 var product = new Product
 {
     ProductName = "PostSharp.Backstage.Settings",
     Solutions = ImmutableArray.Create<Solution>(
-                    new DotNetSolution("PostSharp.Backstage.Settings.sln") { SupportsTestCoverage = true, CanFormatCode = true }),
+                    new DotNetSolution( "PostSharp.Backstage.Settings.sln" ) { SupportsTestCoverage = true, CanFormatCode = true } ),
     PublicArtifacts = Pattern.Create(
         "PostSharp.Backstage.Settings.$(PackageVersion).nupkg",
-        "PostSharp.Cli.$(PackageVersion).nupkg"),
+        "PostSharp.Cli.$(PackageVersion).nupkg" ),
     Dependencies = ImmutableArray.Create(
-        Dependencies.PostSharpEngineering)
+        Dependencies.PostSharpEngineering )
 };
 
 var commandApp = new CommandApp();
 
-commandApp.AddProductCommands(product);
+commandApp.AddProductCommands( product );
 
-return commandApp.Run(args);
+return commandApp.Run( args );
