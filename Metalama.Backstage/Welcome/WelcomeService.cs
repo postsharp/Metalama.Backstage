39476771da541c0ff6a34b30cfacbb1c769553aa
Antonín Procházka <antonin@postsharp.net>
2022-05-27 15:59:54 +0200

    #30377 WelcomeService split to initial configuration and welcome page opening.

--- Metalama.Backstage/Welcome/WelcomeService.cs
+++ Metalama.Backstage/Welcome/WelcomeService.cs
@@ -12,18 +12,26 @@ using System.Diagnostics;
 
 namespace Metalama.Backstage.Welcome;
 
-public static class WelcomeService
+public class WelcomeService
 {
-    public static void Execute( IServiceProvider serviceProvider, bool registerEvaluationLicense = true )
-    {
-        var logger = serviceProvider.GetLoggerFactory().GetLogger( "Welcome" );
-        var configurationManager = serviceProvider.GetRequiredService<IConfigurationManager>();
-        var welcomeConfiguration = configurationManager.Get<WelcomeConfiguration>();
-        var applicationInfo = serviceProvider.GetRequiredService<IApplicationInfo>();
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger _logger;
+    private readonly IConfigurationManager _configurationManager;
+    private readonly WelcomeConfiguration _welcomeConfiguration;
 
-        if ( !welcomeConfiguration.IsFirstStart )
+    public WelcomeService( IServiceProvider serviceProvider )
+    {
+        this._serviceProvider = serviceProvider;
+        this._logger = serviceProvider.GetLoggerFactory().GetLogger( "Welcome" );
+        this._configurationManager = serviceProvider.GetRequiredService<IConfigurationManager>();
+        this._welcomeConfiguration = this._configurationManager.Get<WelcomeConfiguration>();
+    }
+
+    private void ExecuteOnce( Action action, string actionName, Func<WelcomeConfiguration, bool> getIsFirst, Action<WelcomeConfiguration> setIsNotFirst )
+    {
+        if ( !getIsFirst( this._welcomeConfiguration ) )
         {
-            logger.Trace?.Log( "The Welcome service has already been executed." );
+            this._logger.Trace?.Log( $"The {actionName} action has already been executed." );
 
             return;
         }
@@ -31,14 +39,14 @@ public static class WelcomeService
         // We need a global lock to start the welcome service because several process may attempt to start the evaluation
         // license. We need the processes who lost the race to wait, so they can read the configuration file.
 
-        using ( MutexHelper.WithGlobalLock( "Welcome" ) )
+        using ( MutexHelper.WithGlobalLock( $"Welcome{actionName}" ) )
         {
-            if ( !configurationManager.Update<WelcomeConfiguration>(
+            if ( !this._configurationManager.Update<WelcomeConfiguration>(
                     c =>
                     {
-                        if ( c.IsFirstStart )
+                        if ( getIsFirst( c ) )
                         {
-                            c.IsFirstStart = false;
+                            setIsNotFirst( c );
 
                             return true;
                         }
@@ -52,60 +60,81 @@ public static class WelcomeService
             {
                 // Another process has won the race.
 
-                logger.Trace?.Log( "Welcome service has been executed by a concurrent process." );
+                this._logger.Trace?.Log( $"{actionName} action has been executed by a concurrent process." );
 
                 return;
             }
-
-            // Start the evaluation license.
-            if ( registerEvaluationLicense )
-            {
-                var evaluationLicenseRegistrar = new EvaluationLicenseRegistrar( serviceProvider );
-                evaluationLicenseRegistrar.TryActivateLicense();
-            }
-
-            // Activate telemetry.
-            if ( !TelemetryConfiguration.IsOptOutEnvironmentVariableSet() )
-            {
-                logger.Trace?.Log( "Enabling telemetry." );
-
-                configurationManager.Update<TelemetryConfiguration>(
-                    c =>
-                    {
-                        // Enable telemetry except if it has been disabled by the command line.
-
-                        if ( c.ExceptionReportingAction == ReportingAction.Ask )
-                        {
-                            c.ExceptionReportingAction = ReportingAction.Yes;
-                        }
-
-                        if ( c.PerformanceProblemReportingAction == ReportingAction.Ask )
-                        {
-                            c.PerformanceProblemReportingAction = ReportingAction.Yes;
-                        }
-
-                        if ( c.ReportUsage == ReportingAction.Ask )
-                        {
-                            c.ReportUsage = ReportingAction.Yes;
-                        }
-                    } );
-            }
-
-            // Open the welcome web page.
-            try
-            {
-                var url = applicationInfo.IsPrerelease
-                    ? "https://www.postsharp.net/links/metalama-welcome-preview"
-                    : "https://www.postsharp.net/links/metalama-welcome";
-
-                logger.Trace?.Log( $"Opening '{url}'." );
-
-                Process.Start( new ProcessStartInfo( url ) { UseShellExecute = true } );
-            }
-            catch ( Exception e )
-            {
-                logger.Error?.Log( $"Cannot start the welcome web page: {e.Message}" );
-            }
         }
+    }
+
+    public void ExecuteFirstStartSetup( bool registerEvaluationLicense = true )
+    {
+        this.ExecuteOnce(
+            () =>
+         {
+             // Start the evaluation license.
+             if ( registerEvaluationLicense )
+             {
+                 var evaluationLicenseRegistrar = new EvaluationLicenseRegistrar( this._serviceProvider );
+                 evaluationLicenseRegistrar.TryActivateLicense();
+             }
+
+             // Activate telemetry.
+             if ( !TelemetryConfiguration.IsOptOutEnvironmentVariableSet() )
+             {
+                 this._logger.Trace?.Log( "Enabling telemetry." );
+
+                 this._configurationManager.Update<TelemetryConfiguration>(
+                     c =>
+                     {
+                         // Enable telemetry except if it has been disabled by the command line.
+
+                         if ( c.ExceptionReportingAction == ReportingAction.Ask )
+                         {
+                             c.ExceptionReportingAction = ReportingAction.Yes;
+                         }
+
+                         if ( c.PerformanceProblemReportingAction == ReportingAction.Ask )
+                         {
+                             c.PerformanceProblemReportingAction = ReportingAction.Yes;
+                         }
+
+                         if ( c.ReportUsage == ReportingAction.Ask )
+                         {
+                             c.ReportUsage = ReportingAction.Yes;
+                         }
+                     } );
+             }
+         },
+            nameof( this.ExecuteFirstStartSetup ),
+            c => c.IsFirstStart,
+            c => c.IsFirstStart = false );
+    }
+
+    public void OpenWelcomePage()
+    {
+        this.ExecuteOnce(
+            () =>
+            {
+                try
+                {
+                    var applicationInfo = this._serviceProvider.GetRequiredService<IApplicationInfo>();
+
+                    var url = applicationInfo.IsPrerelease
+                        ? "https://www.postsharp.net/links/metalama-welcome-preview"
+                        : "https://www.postsharp.net/links/metalama-welcome";
+
+                    this._logger.Trace?.Log( $"Opening '{url}'." );
+
+                    Process.Start( new ProcessStartInfo( url ) { UseShellExecute = true } );
+                }
+                catch ( Exception e )
+                {
+                    this._logger.Error?.Log( $"Cannot start the welcome web page: {e.Message}" );
+                }
+            },
+            nameof( this.OpenWelcomePage ),
+            c => c.IsWelcomePagePending,
+            c => c.IsWelcomePagePending = false );
     }
 }
