c8f53c0dbea2ffee49510c25a0ab010860654266
Gael Fraiteur <gael@postsharp.net>
2022-07-14 13:16:29 +0200

    API Cleanup

--- Metalama.Backstage/Extensibility/ApplicationInfoProvider.cs
+++ Metalama.Backstage/Extensibility/ApplicationInfoProvider.cs
@@ -1,15 +1,42 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
+using System;
+
 namespace Metalama.Backstage.Extensibility
 {
     internal class ApplicationInfoProvider : IApplicationInfoProvider
     {
-        public IApplicationInfo CurrentApplication { get; set; }
+        public IApplicationInfo CurrentApplication { get; private set; }
+
+        public IDisposable WithApplication( IApplicationInfo applicationInfo )
+        {
+            var cookie = new WithApplicationDispose( this );
+            this.CurrentApplication = applicationInfo;
+
+            return cookie;
+        }
 
         public ApplicationInfoProvider( IApplicationInfo initialApplication )
         {
             this.CurrentApplication = initialApplication;
+        }
+
+        private class WithApplicationDispose : IDisposable
+        {
+            private readonly ApplicationInfoProvider _parent;
+            private readonly IApplicationInfo _previousApplication;
+
+            public WithApplicationDispose( ApplicationInfoProvider parent )
+            {
+                this._parent = parent;
+                this._previousApplication = parent.CurrentApplication;
+            }
+
+            public void Dispose()
+            {
+                this._parent.CurrentApplication = this._previousApplication;
+            }
         }
     }
 }
--- Metalama.Backstage/Extensibility/IApplicationInfoProvider.cs
+++ Metalama.Backstage/Extensibility/IApplicationInfoProvider.cs
@@ -1,12 +1,16 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
+using System;
+
 namespace Metalama.Backstage.Extensibility
 {
     // TODO: For licensing, we need info about all applications together.
     // TODO: Split IApplicationInfo to application, component and process info.
     public interface IApplicationInfoProvider
     {
-        IApplicationInfo CurrentApplication { get; set; }
+        IApplicationInfo CurrentApplication { get; }
+
+        IDisposable WithApplication( IApplicationInfo applicationInfo );
     }
 }
