3ccffa5b0dbe739716a565b8110dc001438628c2
Gael Fraiteur <gael@postsharp.net>
2022-07-26 09:41:48 +0200

    Error code in hex.

--- Metalama.Backstage/Diagnostics/MiniDumper.cs
+++ Metalama.Backstage/Diagnostics/MiniDumper.cs
@@ -156,7 +156,7 @@ internal class MiniDumper : IMiniDumper
 
                 if ( !bRet )
                 {
-                    this._logger.Error?.Log( $"MiniDumpWriteDump has failed with error code {Marshal.GetLastWin32Error()}." );
+                    this._logger.Error?.Log( $"MiniDumpWriteDump has failed with error code 0x{Marshal.GetLastWin32Error():x8}." );
 
                     return null;
                 }
