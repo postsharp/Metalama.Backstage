08e1ea7e45b556d87951ea0ecbd082cef6cda3c7
Antonín Procházka <antonin@postsharp.net>
2022-09-07 12:38:46 +0200

    #29590 Test license key for integration tests added.

--- Tests/Metalama.Backstage.Tests/TestLicenses.cs
+++ Tests/Metalama.Backstage.Tests/TestLicenses.cs
@@ -74,6 +74,12 @@ namespace Metalama.Backstage.Licensing.Tests
         public const string MetalamaUltimateCommercialRedistribution
             = "16-ZQWQQQQQREAEQCRCE4UW3UFEB4URXMHRB8KQBJJSB64LX7YQ2GYCXBSF629W7YDRH29BN7JFYCJX3MFVVAHZXJ9RS29KYTHFS8KQ7TFRS6ZTZHUAE5D6EGGR7GKBRC4N4BYSAS4MX2JRFGTW3AELYYSMRX29B3S6XLBLCS4C5MG6NSQQBTWB2P7QNG4B27RXP3";
 
+        // This is to be used in Metalama Integration tests, where the test dependency has a random assembly name.
+        public const string MetalamaUltimateOpenSourceRedistributionForIntegrationTestsNamespace = "dependency_XXXXXXXXXXXXXXXX";
+
+        public const string MetalamaUltimateOpenSourceRedistributionForIntegrationTests
+            = "17-ZEWQQQQQXEAXQDW3WQSXSYSRB2KXWJSRX29CXJFVQJKKZJJS564CTFTRS2KCX7GRS68XNK94UZNTSRA4UZNTSRA4UZNTSRA4UBZTZNJZRRFV8N7F9FGWNDF2ZV9Z558VHNDEFCZ94H86VJRXG8JHH5YWXZWGUX6MJK3K8SQQL5AR27W3VJRL5";
+
         public static readonly DateTime SubscriptionExpirationDate = new DateTime( 2050, 1, 1 );
 
         public static string CreateMetalamaFreeLicense( IServiceProvider services )
