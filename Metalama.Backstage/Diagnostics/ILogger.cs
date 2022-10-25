ba73868c4e3a881cd7460e5368d24cf06e6d7f78
GaelFraiteur <gael@postsharp.net>
2022-10-25 10:21:54 +0200

    Fixed warnings.

--- Metalama.Backstage/Diagnostics/ILogger.cs
+++ Metalama.Backstage/Diagnostics/ILogger.cs
@@ -5,25 +5,31 @@ namespace Metalama.Backstage.Diagnostics
     public interface ILogger
     {
         /// <summary>
-        /// Printed to the log file only or to the console in verbose mode.
+        /// Gets the <see cref="ILogWriter"/> for the <c>Trace</c> severity. Messages of this severity are written only in verbose mode.
         /// </summary>
         ILogWriter? Trace { get; }
 
         /// <summary>
-        /// Printed to the console.
+        /// Gets the <see cref="ILogWriter"/> for the <c>Info</c> severity. Messages of this severity are written to the console output as normal text,
+        /// or in logs even when the verbosity for the category is not set to verbose.
         /// </summary>
         ILogWriter? Info { get; }
 
         /// <summary>
-        /// Warning.
+        /// Gets the <see cref="ILogWriter"/> for the <c>Warning</c> severity. Messages of this severity are written to the console output as warnings,
+        /// and to the logs.
         /// </summary>
         ILogWriter? Warning { get; }
 
         /// <summary>
-        /// Error.
+        /// Gets the <see cref="ILogWriter"/> for the <c>Errors</c> severity. Messages of this severity are written to the console output as errors,
+        /// and to the logs.
         /// </summary>
         ILogWriter? Error { get; }
 
+        /// <summary>
+        /// Returns a new <see cref="ILogger"/> for a sub-category.
+        /// </summary>
         ILogger WithPrefix( string prefix );
     }
 }
