3326d6948903173948ba9d85952fe983c2281cea
GaelFraiteur <gael@postsharp.net>
2023-01-08 10:26:26 +0100

    Fixing warnings.

---
+++ Metalama.Backstage.Tool/ApplicationInfo.cs
@@ -0,0 +1,8 @@
+﻿using Metalama.Backstage.Extensibility;
+
+internal class TestToolApplicationInfo : ApplicationInfoBase
+{
+    public TestToolApplicationInfo() : base( typeof(TestToolApplicationInfo).Assembly ) { }
+
+    public override string Name => typeof(TestToolApplicationInfo).Assembly.GetName().Name!;
+}
