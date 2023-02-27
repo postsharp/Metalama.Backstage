7d5d4b4fe321e10e9312b3221adbb73920c88283
GaelFraiteur <gael@postsharp.net>
2023-02-27 12:11:18 +0100

    Changed ITempFileManager signature.

--- Metalama.Backstage/Maintenance/ITempFileManager.cs
+++ Metalama.Backstage/Maintenance/ITempFileManager.cs
@@ -1,11 +1,18 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using Metalama.Backstage.Extensibility;
-using System;
 
 namespace Metalama.Backstage.Maintenance;
 
 public interface ITempFileManager : IBackstageService
 {
-    string GetTempDirectory( string subdirectory, CleanUpStrategy cleanUpStrategy, Guid? guid = null, bool versionNeutral = false );
+    /// <summary>
+    /// Gets a temporary directory and creates it if it does not exist yet.
+    /// </summary>
+    /// <param name="directory">The principal name of the directory, before the version number.</param>
+    /// <param name="cleanUpStrategy">The <see cref="CleanUpStrategy"/>.</param>
+    /// <param name="subdirectory">An optional directory name after the version number.</param>
+    /// <param name="versionNeutral">Indicates whether the Metalama version number should be included in the path.</param>
+    /// <returns></returns>
+    string GetTempDirectory( string directory, CleanUpStrategy cleanUpStrategy, string? subdirectory = null, bool versionNeutral = false );
 }
