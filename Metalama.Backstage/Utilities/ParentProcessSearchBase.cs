e9eada8e3c97afa76103a1d86f674eea4188cc40
Antonín Procházka <antonin@postsharp.net>
2024-05-02 17:38:27 +0200

    #34908 GitHub Actions unattended build detection fixed on Linux, unattended detection added to Mac.

---
+++ Metalama.Backstage/Utilities/ParentProcessSearchBase.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Backstage.Diagnostics;
+using System.Collections.Generic;
+
+namespace Metalama.Backstage.Utilities;
+
+internal abstract class ParentProcessSearchBase
+{
+    public abstract IReadOnlyList<ProcessInfo> GetParentProcesses( ISet<string>? pivots = null );
+    
+    protected ILogger Logger { get; }
+
+    protected ParentProcessSearchBase( ILogger logger )
+    {
+        this.Logger = logger;
+    }
+}
---
+++ Metalama.Backstage/Utilities/ParentProcessSearchBase`1.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Backstage.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Metalama.Backstage.Utilities;
+
+#pragma warning disable SA1649 // File name should match first type name
+internal abstract class ParentProcessSearchBase<TProcessHandle> : ParentProcessSearchBase
+#pragma warning restore SA1649
+{
+    protected ParentProcessSearchBase( ILogger logger ) : base( logger ) { }
+
+    public override IReadOnlyList<ProcessInfo> GetParentProcesses( ISet<string>? pivots = null )
+    {
+        var parents = new List<ProcessInfo>();
+        var parentIds = new HashSet<int>();
+        var currentProcessHandle = this.GetCurrentProcessId();
+        var isSelf = true;
+
+        while ( true )
+        {
+            var (imageName, currentProcessId, parentProcessHandle) = this.GetProcessInfo( currentProcessHandle );
+
+            if ( currentProcessId == 0 )
+            {
+                break;
+            }
+
+            var processInfo = new ProcessInfo( currentProcessId, imageName );
+
+            if ( isSelf )
+            {
+                isSelf = false;
+            }
+            else
+            {
+                if ( !parentIds.Add( processInfo.ProcessId ) )
+                {
+                    // There is a loop.
+                    break;
+                }
+
+                if ( parents.Count > 64 )
+                {
+                    throw new InvalidOperationException(
+                        string.Format(
+                            CultureInfo.InvariantCulture,
+                            "Cannot have more than 64 parents. Parent processes: {0}.",
+                            string.Join( ", ", parents.Select( pi => pi.ProcessId.ToString( CultureInfo.InvariantCulture ) ).ToArray() ) ) );
+                }
+
+                parents.Add( processInfo );
+
+                if ( pivots != null && processInfo.ProcessName != null && pivots.Contains( processInfo.ProcessName ) )
+                {
+                    break;
+                }
+            }
+            
+            this.CloseProcessHandle( currentProcessHandle );
+
+            currentProcessHandle = parentProcessHandle;
+        }
+        
+        this.CloseProcessHandle( currentProcessHandle );
+
+        return parents;
+    }
+
+    protected abstract TProcessHandle GetCurrentProcessId();
+
+    protected abstract (string? ImageName, int CurrentProcessId, TProcessHandle ParentProcessHandle) GetProcessInfo( TProcessHandle processHandle );
+    
+    protected abstract void CloseProcessHandle( TProcessHandle handle );
+}
