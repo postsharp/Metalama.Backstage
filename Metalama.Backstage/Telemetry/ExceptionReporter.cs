0c0755425b384743194770d059e536ad052d5da9
Antonín Procházka <antonin@postsharp.net>
2024-06-28 14:22:07 +0200

    #35137 Tracing of exception reporting improved.

--- Metalama.Backstage/Telemetry/ExceptionReporter.cs
+++ Metalama.Backstage/Telemetry/ExceptionReporter.cs
@@ -242,15 +242,19 @@ internal class ExceptionReporter : IExceptionReporter, IDisposable
                 return;
             }
 
-            this._logger.Trace?.Log( $"Reporting an exception of type {reportedException.GetType().Name}." );
+            this._logger.Trace?.Log( $"Attempting to report an exception of type '{reportedException.GetType().Name}' of kind '{exceptionReportingKind}'." );
 
             if ( exceptionReportingKind == ExceptionReportingKind.Exception )
             {
+                this._logger.Trace?.Log( $"Reporting the exception locally." );
+                
                 this._localExceptionReporter?.ReportException( reportedException, localReportPath );
             }
 
             if ( !this._telemetryConfigurationService.IsEnabled )
             {
+                this._logger.Trace?.Log( $"The exception will not be reported remotely because the telemetry is disabled." );
+                
                 return;
             }
 
@@ -264,6 +268,8 @@ internal class ExceptionReporter : IExceptionReporter, IDisposable
 
                 return;
             }
+            
+            this._logger.Trace?.Log( $"Reporting the exception remotely." );
 
             adapter ??= DefaultExceptionAdapter.Instance;
             var applicationInfo = this._applicationInfoProvider.CurrentApplication;
