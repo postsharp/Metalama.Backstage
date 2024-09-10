85a669c7981e868e97b6896da5253e7106c5b14a
Antonín Procházka <antonin@postsharp.net>
2024-09-10 10:28:49 +0200

    Release 2025.0 initialized.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -5,7 +5,7 @@ using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
 using PostSharp.Engineering.BuildTools.Utilities;
-using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2024_2;
+using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2025_0;
 
 var product = new Product( MetalamaDependencies.MetalamaBackstage )
 {
